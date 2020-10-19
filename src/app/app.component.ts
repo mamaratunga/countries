d97f39d3268992deea814d252f61abd8c1f345af
Amaratunga Mark z003tfvr <mark.amaratunga@siemens.com>
2020-10-19 17:38:06 -0500

    country list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Country } from './country'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'countries';
+  country: Country = { name: "USA" };
+  country2: Country = { name: "Mexico" };
+  country3: Country = { name: "Canada" };
+  countries = [];
+
+
+  getCountries() {
+    if (this.countries.length === 0) {
+      this.countries = [this.country, this.country2, this.country3];
+    }
+    else {
+      this.countries = [];
+    }
+  }
 }
